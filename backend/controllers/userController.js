a589c1326ce841e9c8dfeb844e2681aca15a9e12
GorkemKurtkaya <gorkem.kurtkaya@yahoo.com>
2024-09-19 19:24:13 +0300

    .

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -184,24 +184,6 @@ const createToken = (userId) => {
     });
 }
 
-
-const logoutUser = (req, res) => {
-    // JWT çerezini temizle
-    res.cookie("jwt", "", {
-        maxAge: 0, // Çerezi hemen sil
-        httpOnly: true, // Çerezi yalnızca HTTP istekleri üzerinden erişilebilir kılar
-        path: '/', // Çerezin geçerli olduğu yol
-        sameSite: 'Lax' // Güvenlik için, cross-site isteklerde kullanılabilir
-    });
-
-    // Başarılı yanıt gönder
-    res.status(200).json({
-        succeeded: true,
-        message: "Logged out successfully"
-    });
-};
-
-
 const getAllUsers = async (req, res) => {
     try {
         const users = await User.find({ _id: { $ne: res.locals.user._id } });
@@ -245,4 +227,6 @@ const getAUser=async(req,res)=>{
 
 
 
-export { registerUser, loginUser, createToken, logoutUser, getAllUsers, getAUser,changePassword,addAddress };
+
+
+export { registerUser, loginUser, createToken, getAllUsers, getAUser,changePassword,addAddress };
--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -33,13 +33,12 @@ router.get("/cook", (req, res) => {
 });
 
 router.post('/login', userController.loginUser);
-
 router.route('/:id').get(userController.getAUser);
 router.post('/:userid/purchase/:productid', purchaseProduct);
 router.post("/changePassword", authMiddleWare.authenticateToken, userController.changePassword);
 router.post("/addAddress",authMiddleWare.authenticateToken,userController.addAddress);
 
-// router.route('/dashboard').get(authMiddleWare.authenticateToken, userController.getDashboardPage);
+
 
 
 
