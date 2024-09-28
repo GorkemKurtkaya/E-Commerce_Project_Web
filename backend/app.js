99f5e0b416c436cc9a4d60e6d7a2bba90ef95ddf
GorkemKurtkaya <gorkem.kurtkaya@yahoo.com>
2024-09-28 17:01:26 +0300

    Payment Eklendi

--- backend/app.js
+++ backend/app.js
@@ -49,6 +49,7 @@ app.use("/users", userRoute);
 app.use("/orders", orderRoute);
 app.use("/cart",cartRoute);
 app.use("/product",productRoute);
+app.use("/checkout", stripeRoute);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
